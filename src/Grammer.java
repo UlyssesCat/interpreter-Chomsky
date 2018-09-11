845c8a8d691f467ed252c4186396853dad0b95c1
UlyssesCat <919504937@qq.com>
2018-09-11 17:26:10 +0800

    commit

--- src/Grammer.java
+++ src/Grammer.java
@@ -1,6 +1,8 @@
 import java.util.*;
-
 public class Grammer {
-    set<char> Vn;
-    set<char> Vt;
+    public char[] Vn=new char[10];
+    public char[] Vt=new char[10];
+    public ArrayList<Pri> p=new ArrayList<>();
+    public char S;
+    public int flag=-1;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.*;
+
 public class Main {
 
-    int Max=15;
+    static int Max=15;
 
 
+    public static void input(ArrayList<Pri> p)
+    {
+        String[] left=new String[Max];
+        String[] right=new String[Max];
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0;;i++)
+        {
+            String tmp=scanner.nextLine();
+            if(tmp=="end")   break;
+            String[] tmp2=tmp.split("->");
+            left[i]=tmp2[0];
+            right[i]=tmp2[1];
+            Pri temp=new Pri(tmp2[0],tmp2[1]);
+            p.add(temp);
+        }
+    }
 
-    public static void main(String[] args) {
+    public static boolean hasVn(String s){
+        String news=s.toLowerCase();
+        if(news.equals(s))
+            return false;
+        else
+            return true;//存在非终结符
+
+    }
+
+    public static void main(String[] args)
+    {
         System.out.println("Hello World!");
+
+        Grammer G=new Grammer();
+        input(G.p);
     }
 
 
